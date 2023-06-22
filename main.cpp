51aa88d08266114678fad7a1cbc79d65d402f6ce
Novak <novaldix@gmail.com>
2023-06-22 20:56:06 +0300

    19_5_1 fixed names and descr to readme

--- main.cpp
+++ main.cpp
@@ -6,35 +6,35 @@ using std::string;
 using std::vector;
 
 int main() {
-    vector<string> list = { "ring", "sleeping", "farmer", "true", "like", "another" };
+    vector<string> words = { "ring", "sleeping", "farmer", "true", "like", "another" };
 
-    const char* path = R"(..\lexicon.txt)";
-    std::ifstream glossary;
-    glossary.open(path);
+    const char* path = R"(..\words.txt)";
+    std::ifstream in;
+    in.open(path);
 
-    printf("%-10s: %s\n", "string", "count");
+    printf("%-10s: %s\n", "word", "count");
     printf("-------------------\n");
 
-    for (const auto &item : list) {
+    for (const auto &word : words) {
         int numberOfFound = 0;
 
-        while (!glossary.eof()) {
+        while (!in.eof()) {
             string currentFromFile;
 
             // Считываем из файла по слову
-            glossary >> currentFromFile;
-            int result = item.compare(currentFromFile);
+            in >> currentFromFile;
+            int result = word.compare(currentFromFile);
             if (result == 0) {
                 ++numberOfFound;
             }
         }
 
-        printf("%-10s: %s\n", item.c_str(), (numberOfFound ? std::to_string(numberOfFound).c_str() : "not found"));
+        printf("%-10s: %s\n", word.c_str(), (numberOfFound ? std::to_string(numberOfFound).c_str() : "not found"));
 
         // Сбросить состояние потока и переместиться в начало файла
-        glossary.clear();
-        glossary.seekg(0, std::ios::beg);
+        in.clear();
+        in.seekg(0, std::ios_base::beg);
     }
 
-    glossary.close();
+    in.close();
 }
